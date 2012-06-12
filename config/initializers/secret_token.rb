7035aaa38ee073db2873a071317e3bcad08d3b29
Dave Walsh <dave2walsh@yahoo.ca>
2012-06-12 15:15:06 -0400

    First commit on project inception

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WordjackTest::Application.config.secret_token = 'a65e8d94e8fd45232fb56d6482fe65d82343b6afe485ad0ee106f16911a1c57a6ebc5009640dc6759e647114c846b23ef967b109e5ce83f08c35defcabfe9903'
