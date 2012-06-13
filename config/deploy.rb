0f87fcc6f829eca87fdb4dde686df1813347397a
Dave Walsh <dave2walsh@yahoo.ca>
2012-06-13 16:07:35 -0400

    Set up testing environment for rspec. Add guard and spork.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,39 @@
+require "rvm/capistrano"
+
+#Name of application
 set :application, "mini_test"
+
+#Location of repository
 set :repository,  "git@github.com:dave2walsh/mini_project.git"
 
+#Repository type
 set :scm, :git
 
 #Detect rubyversion@gemset gemset used for deployment
 set :rvm_ruby_string, ENV['GEM_HOME'].gsub(/.*\//,"")
 
+#Branch to use for pushing code
 set :branch, "master"
+
+#Only fetch the changes since the last deploy
 set :deploy_via, :remote_cache
 
+#Directory on the server to deploy to
 set :deploy_to, "/home/dave/Rails_Projects/wordjack_test"
 
+#Use local keys instead of those on the server
 ssh_options[:forward_agent] = true
 
-# ssh username
+#ssh username
 set :user, "dave"
-# ssh/sudo password
+
+#ssh/sudo password
 set :password, "the_daddyo"
 
 #Use this to avoid "no tty present and no askpass program specified" error
 default_run_options[:pty] = true
 
-#Only keep the last five latest releases, otherwise the server gets too full of old releases. Works with cap deploy:cleanup
+#Only keep the last five latest releases, otherwise the server gets too full of old releases. Works with bundle exec cap deploy:cleanup
 set :keep_releases, 5
 
 #Deoployment locations
@@ -29,15 +41,14 @@ role :web, "192.168.2.7"                          # Your HTTP server, Apache/etc
 role :app, "192.168.2.7"                          # This may be the same as your `Web` server
 role :db,  "192.168.2.7", :primary => true # This is where Rails migrations will run
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+#Clean up old releases on each deploy
+after "deploy:restart", "deploy:cleanup"
 
+#Order in which tasks are executed
 after "deploy", "deploy:bundle_gems"
 after "deploy:bundle_gems", "deploy:restart"
 
-require "rvm/capistrano"
-
-#Run bundle, restart nginx
+#Run bundle. Restart nginx without breaking any current connections
 namespace :deploy do
   task :bundle_gems do
     run "cd #{deploy_to}/current && bundle install"
